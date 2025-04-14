4680d4b7dd7606f5c3e8db27ec77c29be4462f92
iee2019002 <iee2019002@iee.teithe.gr>
2025-04-14 18:24:39 +0300

    Initial Commit

--- app/src/main/java/com/anemoessa/advinttopics2025_athasangelos/ui/theme/Color.kt
+++ app/src/main/java/com/anemoessa/advinttopics2025_athasangelos/ui/theme/Color.kt
@@ -2,10 +2,17 @@ package com.anemoessa.advinttopics2025_athasangelos.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+//Dark Theme
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
+//Light Theme
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//Custom
+val EggplantPurple = Color(0xFF560AF1)
+val PurpleGrey = Color(0xFF4A3B6E)
+val MagentaPink = Color(0xFFCB32E5)
